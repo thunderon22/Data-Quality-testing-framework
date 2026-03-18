c1b0774e0aed6e7387aca2d142fbd40885c08cf0
nagarjunaIntern <nagarjuna.gayam@coschool.ai>
2026-03-18 20:19:03 +0530

    Data completion validation

--- data.py
+++ data.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 df = pd.read_csv("Datasets/dirty_cafe_sales.csv")
 
-print(df.shape)          # how many rows and columns
-print(df.dtypes)         # what types pandas detected
-print(df.columns.tolist())  # exact column names
-print(df.head(3))        # first 3 rows
+# print(df.shape)          # how many rows and columns
+# print(df.dtypes)         # what types pandas detected
+# print(df.columns.tolist())  # exact column names
+# print(df.head(3))        # first 3 rows
+print(df.isnull().sum())
